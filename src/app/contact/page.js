04da6182b4489eba55f87bba30a95000eb64e9e1
Carey <cjburn6@gmail.com>
2024-06-06 21:31:00 -0400

    updated contact file

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function ContactPage() {
   return (
-    <div>Contact Page:
+    <div><strong>Contact Page:</strong>
 Phone:
 910.233.2369
 Email:
@@ -11,6 +11,29 @@ cjburn6@gmail.com
 Location:
 Raleigh, NC
     </div>
+    
+    <div class="container">
+    <form action="action_page.php">
+
+      <label for="fname">First Name</label>
+      <input type="text" id="fname" name="firstname" placeholder="Your first name..">
+
+      <label for="lname">Last Name</label>
+      <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+
+      <label for="email">Email</label>
+      <input type="text" id="email" name="email" placeholder="Your email..">
+
+      <label for="phone">Phone</label>
+      <input type="text" id="phone" name="phone" placeholder="Your phone number..">
+
+      <label for="subject">Comments</label>
+      <textarea id="comment" name="comment" placeholder="Write something.." style="height:200px"> </textarea>
+
+      <input type="submit" value="Submit">
+
+    </form>
+  </div>
   )
 }
 
